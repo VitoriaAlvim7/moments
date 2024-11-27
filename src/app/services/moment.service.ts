6bee92d89a8c3ad0bf8152a23ec6835dde79311f
Vitoria <2411550024@fema.edu.br>
2024-11-27 09:17:01 -0300

    feat: resgatar dados do banco

--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Moment } from '../Moment';
 import { environment } from 'src/environments/environment';
+import { Response } from '../response';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,10 @@ export class MomentService {
   private apiUrl = `${this.baseApiUrl}api/moments`
 
   constructor(private http: HttpClient) { }
+
+  getMoments(): Observable<Response<Moment[]>> {
+    return this.http.get<Response<Moment[]>>(this.apiUrl);
+  }
 
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);
